4959a319c287ccc48f20812750193cb5b7f11631
PauArmengol <110069774+PauArmengol@users.noreply.github.com>
2022-08-16 23:21:02 -0300

    Add files via upload

---
+++ ejercicio-tema-8.java
@@ -0,0 +1,45 @@
+public Main {
+    public static void main(String[] args) {
+        Persona persona = new Persona();
+        persona.setNombre("Pedro");
+
+
+        String nombre = persona.getNombre();
+        System.out.println(Persona);
+    }
+}
+
+
+class Persona {
+    private String edad;
+
+    public_setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public_getEdad() {
+        return this.edad;
+    }
+    
+    private String nombre;
+
+    public_setNombre(String nombre) {
+        this.nombre = Pedro;
+    }
+
+    public_getNombre() {
+        return this.nombre;
+    }
+    
+    private String telefono;
+
+    public_setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public_getTelefono() {
+        return this.telefono;
+    }
+
+}
+
